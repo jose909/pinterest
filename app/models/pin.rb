bc1f40c9ea5fe0d59a6fcf98f68a808cc678f77d
jose909 <rosaleayal19@hotmail.com>
2016-03-17 17:02:51 -0600

    add new phone field and add new page my pins

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,4 +2,6 @@ class Pin < ActiveRecord::Base
 
 	validates :photo, presence:true
 	validates :description, presence:true
+
+	belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160317155544) do
+ActiveRecord::Schema.define(version: 20160317213737) do
 
   create_table "pins", force: :cascade do |t|
     t.string   "photo"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20160317155544) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "name"
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -34,6 +35,7 @@ ActiveRecord::Schema.define(version: 20160317155544) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "phone"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
